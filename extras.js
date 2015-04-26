cef3a964d6e8a829f75762f5f1b70f7dc54817ec
clement <maboiteaspam@wanadoo.fr>
2015-04-26 21:18:18 +0800

    improve extra example

--- extras.js
+++ extras.js
@@ -12,30 +12,27 @@ module.exports = {
         });
       },
       uninstall: function(){
-        return this.stream('sudo apt-get clean', function(){
-          //this.display();
-        }).stream('sudo apt-get remove apache2 -y', function(){
+        return this.stream('sudo apt-get remove apache2 -y', function(){
           this.progress(/Reading (:<title>[\w\s]+)[ .]*(:<current>\d+)%/);
           this.mustnot(/0 to remove/, 'no package to remove found');
           this.confirm(/([1-9]+) to remove/, ' found %s package to remove');
           this.success(/(Removing [^ ]+\s+[.]+)/i, 'Package removed !');
           this.warn(/(Unable to locate package )/i, 'Package not found');
+        }).stream('sudo apt-get clean', function(){
+          //this.display();
         });
       },
       install: function(){
         return this.stream('sudo apt-get install apache2 -y', function(){
           this.mustnot(/You should explicitly select one to install/, "too many results");
           this.mustnot(/has no installation candidate/, "can not install httpd");
-          this.watch(/Need to get ([0-9- ,-]+ [a-z]+) of archives/i);
-          this.watch(/After this operation, ([0-9- ,-]+ [a-z]+) of additional disk space will be used/i);
-          this.spin(/(Reading package lists.+)/i);
-          this.spin(/(Building dependency tree.+)/i);
-          this.spin(/(Reading state information.+)/i);
-          this.spin(/^\s*([0-9]+%)/i);
+          this.watch(/Need to get ([0-9- ,-]+ [a-z]+) of archives/i, "downloading %s");
+          this.watch(/([0-9- ,.]+\s+[a-z]+) of additional disk space will be used/i, "consumed %s");
+          this.spin(/[.]+\s+([0-9]+)%/);
           this.warn(/(is already the newest version)/i, 'Already installed');
           this.warn(/(0 newly installed)/i, 'Package not installed');
-          this.confirm(/([^ /]+)\.deb/i);
-          this.success(/(1 newly installed)/i, 'Package installed !');
+          this.confirm(/([^ /]+)\.deb/i, "version is %s");
+          this.success(/(Setting up)/i, 'Package installed !');
           //this.display();
         });
       },
